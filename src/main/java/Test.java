9bdfbb88a5addad40ff89fd4548aac954d3600fa
vikasachari109-Git <vikas.achari109@gmail.com>
2022-03-30 16:39:34 +0530

    Added Test Class

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello To Jenkins Integration");
+        System.out.println("Hello To Jenkins Integrations");
     }
 }
